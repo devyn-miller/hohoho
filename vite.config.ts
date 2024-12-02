8bae6832b6f2ea1d92482c6578fed2b164ed53c8
devyn-miller <144622580+devyn-miller@users.noreply.github.com>
2024-12-01 21:51:28 -0800

    Setup GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/naughty-nice-chatbot-dec-1-2024/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
